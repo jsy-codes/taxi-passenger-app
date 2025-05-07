1b01a1ab9d28c320d1722b5e739d6adae63a569b
cornCar <port9746@gmail.com>
2025-05-07 12:55:04 +0900

    first initializer pack

---
+++ src/test/java/com/EONET/eonet/EonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EONET.eonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
