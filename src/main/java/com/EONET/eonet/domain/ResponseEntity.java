87df8c1f1a5ea4df27153d3a0e1dc05a47c10bbd
Imyongjunn <juny0505@naver.com>
2025-05-28 11:06:44 +0900

    TaxiParticipantEntity.java commit TaxiParticipantController.java commit TaxiParticipantRepository.java commit

--- src/main/java/com/EONET/eonet/domain/ResponseEntity.java
+++
@@ -1,7 +0,0 @@
-package com.EONET.eonet.domain;
-
-@PostMapping
-public ResponseEntity<TaxiPost> createPost(@RequestBody TaxiPost post) {
-        TaxiPost saved = taxiPostRepository.save(post);
-        return ResponseEntity.ok(saved);
-        }
--- src/main/java/com/EONET/eonet/domain/TaxiParticipantEntity.java
+++ src/main/java/com/EONET/eonet/domain/TaxiParticipantEntity.java
@@ -1,5 +1,11 @@
 package com.EONET.eonet.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
 
 @Entity
 public class TaxiParticipant {
